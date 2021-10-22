a6c7029d2801a0c8485e39c20ca95e5e4ae27a90
shalin <shalin.rono@student.moringaschool.com>
2021-10-22 23:30:57 +0300

    create class Profile

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from cloudinary.models import CloudinaryField
+from django.db.models.signals import pre_delete
+import cloudinary
+import datetime as dt
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    bio = models.TextField(null=True)
+    profile_photo = CloudinaryField('profile_photo', null=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
