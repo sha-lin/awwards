bd58cf8729bb05ec568cb2d12f7f4829b170a5ea
shalin <shalin.rono@student.moringaschool.com>
2021-10-23 11:58:17 +0300

    create form for uploading a project and rating a project

--- awardsapp/forms.py
+++ awardsapp/forms.py
@@ -21,3 +21,21 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['profile_photo', 'bio']
+
+class ProjectUploadForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ['title', 'project_image', 'description', 'link' ]
+
+    def form_valid(self, form):
+        form.instance.user = self.request.profile
+        return super().form_valid(form)
+
+class RateForm(forms.ModelForm):
+    rate_design = forms.ChoiceField(choices=RATE_CHOICES, widget = forms.Select(), required = True)
+    rate_usability = forms.ChoiceField(choices=RATE_CHOICES, widget = forms.Select(), required = True)
+    rate_content = forms.ChoiceField(choices=RATE_CHOICES, widget = forms.Select(), required = True)
+
+    class Meta:
+        model = Rating
+        fields = ['rate_design', 'rate_usability', 'rate_content' ]
